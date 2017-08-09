b804ba28ba4bc6d1740abbfd1d02c52247e12e56
Sergey Ryabov <connacte@gmail.com>
2017-08-09 22:13:38 +0300

    Add logic for add for simple case

--- radix_tree.rb
+++ radix_tree.rb
@@ -54,9 +54,34 @@ end
 
 
 class RadixTree
-  def initialize(tree)
-    @tree = tree
+  def initialize(array_of_words_with_values)
+    @root = Node.new([])
+    @tree = [@root]
+    @index_counter = 0
+
+    fill_tree(array_of_words_with_values)
   end
+
+  def tree
+    @tree
+  end
+
+  def add(word, value)
+    in_tree = has?(word)
+
+    if in_tree
+      # logic
+    else
+      new_node_index = @index_counter += 1
+
+      edge = Edge.new(word, value, new_node_index)
+      node = Node.new(new_node_index, [])
+
+      @root.add_edge(edge)
+      @tree << node
+    end
+  end
+
 
   def has?(word)
     next_edge, traverse_node, elements_found = search(word)
@@ -107,24 +132,32 @@ class RadixTree
     n = word.length - elements_found
     word.last(n)
   end
+
+  def fill_tree(array_of_words_with_values)
+    array_of_words_with_values.each do |hash|
+      hash.each do |k,v|
+        add(k, v)
+      end
+    end
+  end
 end
 
 
            -> edge(rub, root)
-                                             -> edge(us, [2]) -> node(2)
-node(root) -> edge(roman, [1]) -> node(1)
-                                             -> edge(e, [2]) -> node(2)
+                                             -> edge(us, [2], '1') -> node(2)
+node(root) -> edge(roman, [1], nil) -> node(1)
+                                             -> edge(e, [2], '2') -> node(2)
 
 
 
-node(root) -> edge(t, [next_node_index, next_node_index]) ->
+node(root, 0) -> edge(romanus,[1]) -> node(1) -> edge
 
 node_index = 0
 root = Node.new([])
 
 node_index += 1
 
-first_edge = Edge.new('roman', '0', node_index)
+first_edge = Edge.new('roman', nil, node_index)
 root.add_edge(first_edge)
 
 first_node = Node.new(node_index, [])
@@ -156,5 +189,29 @@ class String
 
   def from(position)
     self[position..-1]
+  end
+
+  def intersection(str)
+    return '' if [self, str].any?(&:empty?)
+
+    matrix = Array.new(self.length) { Array.new(str.length) { 0 } }
+
+    intersection_length = 0
+    intersection_end    = 0
+
+    self.length.times do |x|
+      str.length.times do |y|
+        next unless self[x] == str[y]
+        matrix[x][y] = 1 + (([x, y].all?(&:zero?)) ? 0 : matrix[x-1][y-1])
+
+        next unless matrix[x][y] > intersection_length
+        intersection_length = matrix[x][y]
+        intersection_end    = x
+      end
+    end
+
+    intersection_start = intersection_end - intersection_length + 1
+
+    slice(intersection_start..intersection_end)
   end
 end
